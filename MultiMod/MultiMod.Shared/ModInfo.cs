284b2aca691a3f733c72949dcc472c58f6e9f2db
Dustin Lacewell <dlacewell@gmail.com>
2019-04-19 01:41:40 -0500

    Undo unserializable refactor???

--- MultiMod/MultiMod.Shared/ModInfo.cs
+++ MultiMod/MultiMod.Shared/ModInfo.cs
@@ -5,29 +5,117 @@ using UnityEngine;
 namespace MultiMod.Shared
 {
     /// <summary>
-    ///     Class that stores a Mod's name, author, description, version, path and supported platforms.
+    /// Class that stores a Mod's name, author, description, version, path and supported platforms.
     /// </summary>
     [Serializable]
     public class ModInfo
     {
-        [SerializeField] private readonly string _author;
-
-        [SerializeField] private readonly ModContent _content;
-
-        [SerializeField] private readonly string _description;
-
-        [SerializeField] private bool _isEnabled;
-
-        [SerializeField] private readonly string _name;
-
-        [SerializeField] private readonly ModPlatform _platforms;
-
-        [SerializeField] private readonly string _unityVersion;
-
-        [SerializeField] private readonly string _version;
+        /// <summary>
+        /// Name
+        /// </summary>
+        public string name {
+            get {
+                return _name;
+            }
+        }
 
         /// <summary>
-        ///     Initialize a new ModInfo.
+        /// Supported platforms for this mod.
+        /// </summary>
+        public ModPlatform platforms {
+            get {
+                return _platforms;
+            }
+        }
+
+        /// <summary>
+        /// The Mod's available content types.
+        /// </summary>
+        public ModContent content {
+            get {
+                return _content;
+            }
+        }
+
+        /// <summary>
+        /// Mod author.
+        /// </summary>
+        public string author {
+            get {
+                return _author;
+            }
+        }
+
+        /// <summary>
+        /// Mod description.
+        /// </summary>
+        public string description {
+            get {
+                return _description;
+            }
+        }
+
+        /// <summary>
+        /// Mod version.
+        /// </summary>
+        public string version {
+            get {
+                return _version;
+            }
+        }
+
+        /// <summary>
+        /// The version of Unity that was used to export this mod.
+        /// </summary>
+        public string unityVersion {
+            get {
+                return _unityVersion;
+            }
+        }
+
+        /// <summary>
+        /// Should this mod be enabled.
+        /// </summary>
+        public bool isEnabled {
+            get {
+                return _isEnabled;
+            }
+            set {
+                _isEnabled = value;
+            }
+        }
+
+        /// <summary>
+        /// Location of mod
+        /// </summary>
+        public string path { get; private set; }
+
+        [SerializeField]
+        private string _name;
+
+        [SerializeField]
+        private string _author;
+
+        [SerializeField]
+        private string _description;
+
+        [SerializeField]
+        private string _version;
+
+        [SerializeField]
+        private string _unityVersion;
+
+        [SerializeField]
+        private ModPlatform _platforms;
+
+        [SerializeField]
+        private ModContent _content;
+
+        [SerializeField]
+        private bool _isEnabled;
+
+        /// <summary>
+        /// Initialize a new ModInfo.
         /// </summary>
         /// <param name="name">The Mod's name.</param>
         /// <param name="author">The Mod's author.</param>
@@ -57,56 +145,7 @@ namespace MultiMod.Shared
         }
 
         /// <summary>
-        ///     Name
-        /// </summary>
-        public string name => _name;
-
-        /// <summary>
-        ///     Supported platforms for this mod.
-        /// </summary>
-        public ModPlatform platforms => _platforms;
-
-        /// <summary>
-        ///     The Mod's available content types.
-        /// </summary>
-        public ModContent content => _content;
-
-        /// <summary>
-        ///     Mod author.
-        /// </summary>
-        public string author => _author;
-
-        /// <summary>
-        ///     Mod description.
-        /// </summary>
-        public string description => _description;
-
-        /// <summary>
-        ///     Mod version.
-        /// </summary>
-        public string version => _version;
-
-        /// <summary>
-        ///     The version of Unity that was used to export this mod.
-        /// </summary>
-        public string unityVersion => _unityVersion;
-
-        /// <summary>
-        ///     Should this mod be enabled.
-        /// </summary>
-        public bool isEnabled
-        {
-            get => _isEnabled;
-            set => _isEnabled = value;
-        }
-
-        /// <summary>
-        ///     Location of mod
-        /// </summary>
-        public string path { get; private set; }
-
-        /// <summary>
-        ///     Save this ModInfo.
+        /// Save this ModInfo.
         /// </summary>
         public void Save()
         {
@@ -115,19 +154,21 @@ namespace MultiMod.Shared
         }
 
         /// <summary>
-        ///     Save a ModInfo.
+        /// Save a ModInfo.
         /// </summary>
         /// <param name="path">The path to save the ModInfo to.</param>
         /// <param name="modInfo">The ModInfo to save.</param>
         public static void Save(string path, ModInfo modInfo)
         {
-            var json = JsonUtility.ToJson(modInfo, true);
+            string json = JsonUtility.ToJson(modInfo, true);
+            Debug.Log("JSON:");
+            Debug.Log(json);
 
             File.WriteAllText(path, json);
         }
 
         /// <summary>
-        ///     Load a ModInfo.
+        /// Load a ModInfo.
         /// </summary>
         /// <param name="path">The path to load the ModInfo from.</param>
         /// <returns>The loaded Modinfo, if succeeded. Null otherwise.</returns>
@@ -136,21 +177,21 @@ namespace MultiMod.Shared
             path = Path.GetFullPath(path);
 
             if (File.Exists(path))
+            {
                 try
                 {
-                    var json = File.ReadAllText(path);
+                    string json = File.ReadAllText(path);
 
-                    var modInfo = JsonUtility.FromJson<ModInfo>(json);
+                    ModInfo modInfo = JsonUtility.FromJson<ModInfo>(json);
 
                     modInfo.path = path;
 
                     return modInfo;
-                }
-                catch (Exception e)
+                } catch (Exception e)
                 {
-                    LogUtility.LogWarning("There was an issue while loading the ModInfo from " + path + " - " +
-                                          e.Message);
+                    LogUtility.LogWarning("There was an issue while loading the ModInfo from " + path + " - " + e.Message);
                 }
+            }
 
             return null;
         }
